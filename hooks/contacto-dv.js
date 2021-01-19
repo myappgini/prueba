e8463a36c3ba463676d5741e65442c1dd6bab8c4
landinialejandro <landinialejandro@hotmail.com>
2021-01-19 18:41:01 -0300

    prueba para insertar un objeto en DOM

--- hooks/contacto-dv.js
+++ hooks/contacto-dv.js
@@ -1,3 +1,16 @@
 t = 'contacto';
 f = 'user';
 users_dropdown(f, t);
+
+$j(function () {
+    $j('fieldset').append(add_object());
+});
+
+
+function add_object(){
+    const p = $j('<p />',{
+        class:"bg-primary", // utilizaremos la clase de bootstrap para colorear el fondo del objeto
+        text:" this is my DIV",
+    });
+    return p;
+}
