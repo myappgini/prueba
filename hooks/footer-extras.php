8d9b21339e464bcd5e7fb1ac9b2d0aeaea52f8d9
landinialejandro <landinialejandro@hotmail.com>
2021-01-22 22:01:51 -0300

    start to do list

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -3,6 +3,6 @@
 include ('hooks/multipleUpload/scripts.php');
 
 //* load library for box card in Appgini with bootstrap3
-//include ('hooks/box/scripts.php');
+include ('hooks/box/scripts.php');
 
 ?>
